9d332978a3535168913ca1bc8e30e8e19449c73f
Emerson Gomes Nunes <emerson100loko@hotmail.com>
2023-08-03 13:14:50 -0300

    page catalog

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,11 @@
+
+
+export default function SearchBar () {
+
+    return (
+        <form>
+            <input type="text" placeholder="Digite Sua Busca" />
+            <button type="submit">Buscar</button>
+        </form>
+    );
+}
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,9 +1,26 @@
+import './styles.css';
 import Header from "../../components/Header";
+import SearchBar from '../../components/SearchBar';
 
 export default function Catalog() {
 
 
     return (
+        <>
+        
         <Header />
+        <main className="ct-cartalog-main">
+            <section id="ct-catalog-section">
+                <div className="ct-search-bar-container" > 
+                    <SearchBar/>
+                </div>
+
+                <div className="ct-catalog-cards-container">
+                    <div>teste</div>
+
+                </div>
+            </section>
+        </main>
+        </>
     );
 }
